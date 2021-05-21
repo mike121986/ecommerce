d56dc306f3489ddb4f477f7abe2e7349f622a9c5
victor arana <victor@codersfree.com>
2021-05-21 17:54:25 -0500

    first commit

---
+++ app/Http/Controllers/OrderController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Order;
+use Illuminate\Http\Request;
+
+class OrderController extends Controller
+{
+
+    public function show(Order $order){
+        return view('orders.show', compact('order'));
+    }
+
+    public function payment(Order $order){
+
+        $items = json_decode($order->content);
+
+        return view('orders.payment', compact('order', 'items'));
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\SearchController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\ProductController;
+use App\Http\Livewire\ShoppingCart;
+use App\Http\Livewire\CreateOrder;
+
+Route::get('/', WelcomeController::class);
+
+Route::get('search', SearchController::class)->name('search');
+
+Route::get('categories/{category}', [CategoryController::class, 'show'])->name('categories.show');
+
+Route::get('products/{product}', [ProductController::class, 'show'])->name('products.show');
+
+Route::get('shopping-cart', ShoppingCart::class)->name('shopping-cart');
+
+Route::get('orders/create', CreateOrder::class)->middleware('auth')->name('orders.create');
+
+Route::get('orders/{order}', [OrderController::class, 'show'])->name('orders.show');
+
+Route::get('orders/{order}/payment', [OrderController::class, 'payment'])->name('orders.payment');
