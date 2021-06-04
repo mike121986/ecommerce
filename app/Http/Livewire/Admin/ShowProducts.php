8cd4a2c2a456ecf3879f80e65b5aeb436f88ab1c
victor arana <victor@codersfree.com>
2021-06-04 15:52:19 -0500

    actualizacion

---
+++ app/Http/Livewire/Admin/ShowProducts.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Livewire\Admin;
+
+use App\Models\Product;
+use Livewire\Component;
+
+class ShowProducts extends Component
+{
+
+    public $search;
+
+    public function render()
+    {
+
+        $products = Product::paginate(10);
+
+        return view('livewire.admin.show-products', compact('products'))->layout('layouts.admin');
+    }
+}
