06fb5291e165d1f5a45617e0b0ce9db01a2a71ea
victor arana <victor@codersfree.com>
2021-06-18 13:17:49 -0500

    actualizacion

--- app/Http/Livewire/Admin/SizeProduct.php
+++ app/Http/Livewire/Admin/SizeProduct.php
@@ -3,11 +3,53 @@
 namespace App\Http\Livewire\Admin;
 
 use Livewire\Component;
+use App\Models\Size;
 
 class SizeProduct extends Component
 {
+
+    public $product, $name, $open = false;
+
+    public $size, $name_edit;
+
+    protected $rules = [
+        'name' => 'required'
+    ];
+
+    public function save(){
+        $this->validate();
+
+        $this->product->sizes()->create([
+            'name' => $this->name
+        ]);
+
+        $this->product = $this->product->fresh();
+    }
+
+
+    public function edit(Size $size){
+        $this->open = true;
+        $this->size = $size;
+        $this->name_edit = $size->name;
+    }
+
+    public function update(){
+        $this->validate([
+            'name_edit' => 'required'
+        ]);
+
+        $this->size->name = $this->name_edit;
+        $this->size->save();
+
+        $this->product = $this->product->fresh();
+
+        $this->open = false;
+    }
+
     public function render()
     {
-        return view('livewire.admin.size-product');
+        $sizes = $this->product->sizes;
+
+        return view('livewire.admin.size-product', compact('sizes'));
     }
 }
