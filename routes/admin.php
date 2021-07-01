b46d0c8aedb491a8d928fc3c3a4209d76e4cb80a
victor arana <victor@codersfree.com>
2021-07-01 09:38:27 -0500

    crear nuevo layout

--- routes/admin.php
+++ routes/admin.php
@@ -7,6 +7,7 @@ use App\Http\Livewire\Admin\CreateProduct;
 use App\Http\Livewire\Admin\EditProduct;
 
 use App\Http\Controllers\Admin\ProductController;
+use App\Http\Controllers\Admin\CategoryController;
 
 Route::get('/', ShowProducts::class)->name('admin.index');
 
@@ -15,3 +16,5 @@ Route::get('products/create', CreateProduct::class)->name('admin.products.create
 Route::get('products/{product}/edit', EditProduct::class)->name('admin.products.edit');
 
 Route::post('products/{product}/files', [ProductController::class, 'files'])->name('admin.products.files');
+
+Route::get('categories', [CategoryController::class, 'index'])->name('admin.categories.index');
