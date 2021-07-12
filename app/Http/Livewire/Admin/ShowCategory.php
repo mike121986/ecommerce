bb89f61bcda1f72173bef33c4ea0580b9d94ec21
victor arana <victor@codersfree.com>
2021-07-12 12:04:20 -0500

    actualizacion

--- app/Http/Livewire/Admin/ShowCategory.php
+++ app/Http/Livewire/Admin/ShowCategory.php
@@ -3,14 +3,38 @@
 namespace App\Http\Livewire\Admin;
 
 use App\Models\Category;
+use App\Models\Subcategory;
 use Livewire\Component;
+
+use Illuminate\Support\Str;
 
 class ShowCategory extends Component
 {
-    public $category;
+    public $category, $subcategories;
+
+    public $createForm = [
+        'name' => null,
+        'slug' => null,
+        'color' => false,
+        'size' => false
+    ];
 
     public function mount(Category $category){
         $this->category = $category;
+        $this->getSubcategories();
+    }
+
+
+    public function updatedCreateFormName($value){
+        $this->createForm['slug'] = Str::slug($value);
+    }
+
+    public function getSubcategories(){
+        $this->subcategories = Subcategory::where('category_id', $this->category->id)->get();
+    }
+
+    public function save(){
+
     }
 
     public function render()
